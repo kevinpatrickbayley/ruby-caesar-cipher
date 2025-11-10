018f0edc7ffd65aecc627f8343142e20dbf56eec
Kevin Bayley <kevin.p.bayley@gmail.com>
2025-11-10 07:11:12 -0700

    A cipher that supports capital letters

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,9 +1,14 @@
 def caesarCipher(str, shiftFactor)
   str.chars.map do |char|
-    if char.match?(/[A-Za-z]/)
-      char.ord
+    if char >= 'A' && char <= 'Z'
+      base = 'A'.ord
+      (((char.ord - base + shiftFactor) % 26) + base).chr
     else
       char
     end
   end
 end
+
+test = caesarCipher("HELLO!", 5)
+
+puts test
