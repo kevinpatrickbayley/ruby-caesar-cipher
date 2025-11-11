ab62232e35d62e557960448fcf006056a2cf5a13
Kevin Bayley <kevin.p.bayley@gmail.com>
2025-11-10 20:36:32 -0700

    Comment clarifying shift range normalization

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,5 +1,6 @@
 def caesarCipher(str, shiftFactor)
   #Normalize shiftFactor into 0-25 range
+  #In Ruby, -1 % 26 = 25, which is exactly what we want for negative shifts
   shiftFactor %= 26
   str.chars.map do |char|
     if char >= 'A' && char <= 'Z'
